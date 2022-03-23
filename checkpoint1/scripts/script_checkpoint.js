76c83f024e6471f0532a03ca32024a2f869ace3a
dsf7 <89047938+dsf7@users.noreply.github.com>
2022-03-23 20:18:42 -0300

    Add files via upload
    mudanças nos cards

--- checkpoint1/scripts/script_checkpoint.js
+++ checkpoint1/scripts/script_checkpoint.js
@@ -1,36 +1,56 @@
-let botaoCriar = document.getElementById('criar');
+let botaoCriar = document.getElementById("criar");
 
+function criar() {
+  let nomeLugar = document.getElementById("nomeLugar").value;
+  let textoLugar = document.getElementById("textoLugar").value;
+  let linkImagem = document.getElementById("linkImagem").value;
+  let checkbox = document.getElementById("checkbox");
+  let cardsPai = document.querySelector("div");
+  let novoNo = document.createElement("div");
+  let novoCard = document.createElement("div");
+  let newImg = document.createElement("img");
+  let newTitle = document.createElement("h5");
+  newTitle.classList.add("card-title");
+  newTitle.innerHTML = nomeLugar;
+  newImg.setAttribute("src", linkImagem);
+  newImg.classList.add("card-img-top");
+  let newParagraph = document.createElement("p");
+  newParagraph.classList.add("card-text");
+  newParagraph.innerHTML = textoLugar;
+  let imgParagraph = document.createElement("p");
+  let imgLink = document.createElement("a");
+  imgParagraph.appendChild(imgLink);
+  imgLink.setAttribute("href", linkImagem);
+  imgLink.innerHTML = "Link para imagem.";
+  imgLink.classList.add("img-link");
+  let buttonInfo = document.createElement("a");
+  buttonInfo.classList.add("btn");
+  buttonInfo.classList.add("btn-primary");
+  buttonInfo.innerHTML = "Saiba mais sobre esse lugar.";
+  novoCard.classList.add("card-body");
+  novoNo.appendChild(newImg);
+  novoCard.appendChild(newTitle);
+  novoCard.appendChild(newParagraph);
+  novoCard.appendChild(imgParagraph);
+  novoCard.appendChild(buttonInfo);
+  novoNo.appendChild(novoCard);
+  novoNo.classList.add("card");
+  novoNo.classList.add("col-{breakpoint}-auto");
+  novoNo.classList.add("col-sm-4");
+  novoNo.classList.add("col-12");
 
-
-function criar () {
-    let nomeLugar = document.getElementById('nomeLugar').value;
-    let textoLugar = document.getElementById('textoLugar').value;
-    let linkImagem = document.getElementById('linkImagem').value;
-    let checkbox = document.getElementById('checkbox');
-    let cardsPai = document.getElementById("cardsPai");
-    let newCard = `<div class="card col-{breakpoint}-auto col-sm-4 col-12">
-    <img src="${linkImagem}" class="card-img-top" alt="...">
-    <div class="card-body">
-    <h5 class="card-title">${nomeLugar}</h5>
-    <p class="card-text">${textoLugar}</p>
-    <p class="img-link">${linkImagem}</p>
-    <a href="#" class="btn btn-primary">Saiba mais sobre esse lugar</a>
-    </div>
-    </div>`;
-    let novoNo = document.createElement('div');
-    novoNo.innerHTML = newCard;
-    cardsPai.appendChild(novoNo);
+  //   novoNo.innerHTML = newCard;
+  cardsPai.insertBefore(novoNo, cardsPai.childNodes[7]);
 }
 
-botaoCriar.addEventListener('click', function(evento) {
-    evento.preventDefault(); 
-    
-    console.log(nomeLugar.value);
-    console.log(textoLugar.value);
-    console.log(linkImagem.value);
+botaoCriar.addEventListener("click", function (evento) {
+  evento.preventDefault();
 
-    console.log(checkbox.checked);
+  console.log(nomeLugar.value);
+  console.log(textoLugar.value);
+  console.log(linkImagem.value);
 
-    criar();
-} )
+  console.log(checkbox.checked);
 
+  criar();
+});
